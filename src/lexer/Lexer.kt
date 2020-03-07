5c920bf8bb352fe78026911892683345a1b91ec8
gonearewe <cristmactavish@outlook.com>
2020-03-07 19:59:52 +0800

    fix some bugs

--- src/lexer/Lexer.kt
+++ src/lexer/Lexer.kt
@@ -1,21 +1,72 @@
 package lexer
 
+import java.util.regex.Matcher
 import java.util.regex.Pattern
 
-class Lexer private constructor(val source: String) {
+class Lexer private constructor(private val source: String) {
     private val tokens = ArrayList<Token>()
 
     private val matchers = arrayListOf(
-            Pattern.compile("^\\d*").matcher(source),
-            Pattern.compile("^(let|val|var|const|if|else|return|struct|for|when)").matcher(source),
-            Pattern.compile("^([(){}\\[\\]])").matcher(source)
+            Pair<Matcher, String>(Pattern
+                    .compile("^\\d+")
+                    .matcher(source), "number"),
+
+            Pair<Matcher, String>(Pattern
+                    .compile("^(let|val|var|const|if|else|return|struct|for|when|true|false)")
+                    .matcher(source), "keyword"),
+
+            Pair<Matcher, String>(Pattern
+                    .compile("^([(){}\\[\\]])")
+                    .matcher(source), "separator"),
+
+            Pair<Matcher, String>(Pattern
+                    .compile("^(==|=>|=|\\+\\+|\\+|--|->|-|\\*|/|\\|#)")
+                    .matcher(source), "operator"),
+
+            Pair<Matcher, String>(Pattern
+                    .compile("^(a-zA-Z)[\\w_]*")
+                    .matcher(source), "separator"),
     )
 
-    fun tokenize(source: String): Lexer {
-        val lexer = Lexer(source)
-        var start = 0
-        while (start != source.length) {
+    companion object {
+        fun tokenize(source: String): Lexer {
+            val lexer = Lexer(source)
+            var start = 0
+            while (start != source.length) {
+                if (source[start].isWhitespace()) {
+                    start++
+                    continue
+                }
 
+                for ((matcher, type) in lexer.matchers) {
+                    if (matcher.find(start)) {
+                        val end = matcher.end() + 1
+                        lexer.tokens.add(Token(source.substring(start, end), type))
+                        start = end
+                        break
+                    }
+                }
+            }
+
+            return lexer
+        }
+
+    }
+
+    override fun toString(): String {
+        return buildString(tokens.size) {
+            tokens.map {
+                append("(")
+                append(it.type)
+                append(", ")
+                append(it.value)
+                append(")\n")
+            }
         }
     }
+}
+
+
+fun main() {
+    println(Lexer.tokenize("let x=67; val y=x; when(x==y){ return true;}"))
 }
