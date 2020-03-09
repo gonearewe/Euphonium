f47e6cd8b7001c8f3045efd7d3b74b4a3badd58a
gonearewe <cristmactavish@outlook.com>
2020-03-09 13:05:40 +0800

    finish function call expression

--- src/parser/Expression.kt
+++ src/parser/Expression.kt
@@ -1,37 +1,71 @@
 package parser
 
 import lexer.Token
-import lexer.isIntegerOperator
+import lexer.isOperator
 import java.io.File
+import java.util.logging.Logger
 
-open class Expression {
-
-}
-
-class IntegerExpression private constructor(
+class Expression private constructor(
         val operator: Token? = null,
-        val firstOperand: IntegerExpression? = null,
-        val secondExpression: IntegerExpression? = null,
-        val value: Token? = null
-) : Expression() {
-
-
+        val firstOperand: Expression? = null,
+        val secondExpression: Expression? = null,
+        val value: Token? = null,
+        val funcName: Token? = null,
+        val funcParams: List<Expression>? = null
+) {
     companion object {
-        fun of(expr: List<Token>): IntegerExpression {
-            if (expr.size == 1) {
-                return IntegerExpression(value = expr[0])
+        fun of(expr: List<Token>): Expression? {
+            val logger = Logger.getLogger(Expression::javaClass.name)
+            if (expr.isEmpty()) {
+                return null
             }
-            if (isWrappedWithBracket(expr)) {
+            if (expr.size == 1) { // single literal or variable
+                return Expression(value = expr[0])
+            }
+            if (isWrappedWithBracket(expr)) { // the whole expression is wrapped with a pair of brackets
                 return of(expr.subList(1, expr.size - 1))
             }
 
 //            val newExpr = resolveMinus(expr)
             val splitPoint = indexOfLeastPriorityOperator(expr)
-            return IntegerExpression(
+//            if(splitPoint==-1){
+//                logger.info("split point is -1 while tokens are:\n$expr")
+//            }
+
+            if (splitPoint == -1 && expr[0].type == "identifier" && expr[1].value == "(") { // function call expression
+                return funcCallOf(expr)
+            }
+            return Expression(
                     expr[splitPoint],
                     of(expr.subList(0, splitPoint)),
                     of(expr.subList(splitPoint + 1, expr.size))
             )
+        }
+
+        private fun funcCallOf(expr: List<Token>): Expression {
+            val name = expr[0]
+            val params = ArrayList<Expression>()
+            var tokens = ArrayList<Token>()
+            var bracketStack = 0
+            for (i in 2 until expr.size - 1) {
+                bracketStack = when (expr[i].value) {
+                    "(" -> bracketStack + 1
+                    ")" -> bracketStack - 1
+                    else -> bracketStack
+                }
+
+                if (bracketStack == 0 && expr[i].value == ",") {
+                    params.add(of(tokens)!!)
+                    tokens = ArrayList<Token>()
+                } else {
+                    tokens.add(expr[i])
+                }
+            }
+            if (tokens.isNotEmpty()) {
+                params.add(of(tokens)!!)
+            }
+
+            return Expression(funcName = name, funcParams = params)
         }
     }
 
@@ -40,19 +74,28 @@ class IntegerExpression private constructor(
             return value.value + " "
         }
 
+        if (funcName != null) {
+            return buildString {
+                append(funcName.value + "(")
+                funcParams!!.forEachIndexed { index, it ->
+                    append("$it")
+                    if (index != funcParams.size - 1) {
+                        append(", ")
+                    }
+                }
+                append(") ")
+            }
+        }
+
         return buildString {
             append(operator!!.value)
             append(" ")
             if (firstOperand != null) {
                 append(firstOperand.toString())
-            } else {
-                append("null ")
             }
 
             if (secondExpression != null) {
                 append(secondExpression.toString())
-            } else {
-                append("null ")
             }
         }
     }
@@ -73,8 +116,12 @@ class IntegerExpression private constructor(
 
 private fun indexOfLeastPriorityOperator(expr: List<Token>): Int {
     val priorityTable = hashMapOf<String, Int>(
-            "-" to 0, "+" to 0,
-            "*" to 1, "/" to 1
+            "||" to 0, "&&" to 1, "!" to 2,
+            "==" to 3, "!=" to 3,
+            ">" to 4, "<" to 4, ">=" to 4, "<=" to 4,
+            "!" to 5,
+            "-" to 6, "+" to 6,
+            "*" to 7, "/" to 7
     )
 
     var res = -1
@@ -93,15 +140,14 @@ private fun indexOfLeastPriorityOperator(expr: List<Token>): Int {
             continue
         }
 
-        if (token.isIntegerOperator()) {
-            if (priorityTable[token.value]!! < currentPriority) {
+        if (token.isOperator()) {
+            if (priorityTable[token.value]!! <= currentPriority) {
                 currentPriority = priorityTable[token.value]!!
                 res = index
             }
         }
     }
 
-    assert(res >= 0)
     return res
 }
 
@@ -126,13 +172,9 @@ private fun isWrappedWithBracket(expr: List<Token>): Boolean {
 }
 
 fun main() {
-    val lines = File("D:\\MyProjects\\Aurora\\test\\resource\\parser.txt").readLines()
+    val lines = File("D:\\MyProjects\\Aurora\\test\\resource\\VariableAndFuncCallExpression.txt").readLines()
     var i = 0
     while (i != lines.size) {
-        if (i != 31) {
-            i++
-            continue
-        }
         val line = lines[i]
         if (line.startsWith("#") || line.isBlank()) {
             i++
@@ -141,12 +183,23 @@ fun main() {
 
         val case = ArrayList<Token>()
         line.split(" ").forEach {
-            case.add(Token(it, ""))
+            val type = when {
+                it == "(" || it == ")" || it == "," -> "separator"
+                it[0].isLetter() -> "identifier"
+                else -> ""
+            }
+            case.add(Token(it, type))
         }
 
         val nextLine = lines[i + 1]
         // TEST
-        assert(IntegerExpression.of(case).toString() == nextLine)
+        val output = Expression.of(case).toString().trim() // trim spaces
+        if (output != nextLine) {
+            println("input: $line")
+            println("expecting: $nextLine (string len: ${nextLine.length} )")
+            println("found: $output (string len: ${output.length} )")
+            return
+        }
         // TEST
         i += 2
     }
