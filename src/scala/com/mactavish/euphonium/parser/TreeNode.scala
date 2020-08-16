83fa38fc8a0208a0633b2b916d6e786f0a26e882
gonearewe <cristmactavish@outlook.com>
2020-08-16 21:15:17 +0800

    continue

--- src/scala/com/mactavish/euphonium/parser/TreeNode.scala
+++ src/scala/com/mactavish/euphonium/parser/TreeNode.scala
@@ -1,34 +1,34 @@
 package com.mactavish.euphonium.parser
 
-sealed abstract class TreeNode
+sealed trait TreeNode
 
-case class MethodDef(name:OrdinaryIdent, params:List[VarDef], ret:TypeLiteral, body:Block)
+sealed trait Expr extends TreeNode
+sealed trait Statement extends Expr
+sealed trait Def extends Statement
 
-case class VarDef(name:OrdinaryIdent, typ:TypeLiteral, init:Option[Expr])
+case class MethodDef(name:OrdinaryIdent, params:List[LocalVarDef], ret:TypeLiteral, body:Expr) extends Def
+case class FieldDef(name:OrdinaryIdent, typ:TypeLiteral, init:Option[Expr]) extends Def
+case class LocalVarDef(name:OrdinaryIdent, typ:TypeLiteral, init:Option[Expr]) extends Def
+case class ClassDef(name: TypeIdent,fields:FieldDef,methods:MethodDef) extends Def
 
-case class ClassDef(name: TypeIdent,)
-
-sealed abstract class Identifier(literal:String)
-
-case class TypeIdent(literal:String) extends Identifier(literal)
-
-case class OrdinaryIdent(literal:String) extends Identifier(literal)
-
-sealed trait Expr
-trait Statement extends Expr
+sealed trait Identifier extends TreeNode
+case class TypeIdent(literal:String) extends Identifier
+case class OrdinaryIdent(literal:String) extends Identifier with Expr
 
 case class Binary(op:BinaryOp, operands:(Expr,Expr)) extends Expr
+case class Unary(op:UnaryOp,operand:Expr) extends Expr
 case class Block(expressions:List[Expr]) extends Expr
-
+case class If(condition:Expr,passExpr:Expr,elseExpr:Expr) extends Expr
 
 sealed trait Literal
-trait ValueLiteral[T] extends Literal with Expr{
+sealed trait ValueLiteral[T] extends Literal with Expr{
   val value:T
 }
-trait TypeLiteral extends Literal
+sealed trait TypeLiteral extends Literal
 
 case class IntLit(value:Int) extends ValueLiteral[Int]
 case class BoolLit(value:Boolean) extends ValueLiteral[Boolean]
+case class StringLit(value: String) extends ValueLiteral[String]
 
 case class TInt() extends TypeLiteral
 case class TBool() extends TypeLiteral
@@ -36,10 +36,19 @@ case class TString() extends TypeLiteral
 case class TUnit() extends TypeLiteral
 
 sealed trait Op
-trait BinaryOp extends Op
-trait UnaryOp extends Op
+sealed trait BinaryOp extends Op
+sealed trait UnaryOp extends Op
 
 case class ADD() extends BinaryOp
 case class SUB() extends BinaryOp
 case class MUL() extends BinaryOp
 case class DIV() extends BinaryOp
+case class EQ() extends BinaryOp
+case class NE() extends BinaryOp
+case class LT() extends BinaryOp
+case class GT() extends BinaryOp
+case class AND() extends BinaryOp
+case class OR() extends BinaryOp
+
+case class NEG() extends UnaryOp
+case class NOT() extends UnaryOp
